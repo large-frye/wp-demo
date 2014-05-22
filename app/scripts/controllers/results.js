9d4ecd65f6c59777ec68e0a95b6691e02b0e439c
Christian Lilley <christian_lilley@mckinsey.com>
2014-05-22 10:51:44 -0400

    use params to perform the search

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('waPoDemoApp')
-  .controller('ResultsCtrl', function ($scope, $routeParams) {
-    console.log($routeParams);
+    .controller('ResultsCtrl', function($scope, $routeParams, API) {
+        console.log($routeParams);
+        // $scope.resultData = 
 
-  });
+        $scope.searchWithParams = function() {
+            API.openAPI($routeParams.uuid, $routeParams.days);
+        }
+    });
