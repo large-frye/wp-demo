c7afc07afc67b2fd6a130b63b37ead830d1ac9ed
Christian Lilley <christian_lilley@mckinsey.com>
2014-05-22 10:09:56 -0400

    put form on model, show sub-controller presence on $scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 angular.module('waPoDemoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $location, API) {
+
+    // console.log($scope.searchForm);
+
+    $scope.searchParams = {};
+
+    $scope.$watch('searchForm', function(newVal, oldVal) {
+      console.log(newVal);
+    });
+    $scope.$watch('searchForm.uuidField', function(newVal, oldVal) {
+      console.log(newVal);
+    });
+    $scope.$watch('searchForm.daysField', function(newVal, oldVal) {
+      console.log(newVal);
+    });
+
+    $scope.buttonTest = function() {
+        console.log($scope.searchForm.uuidField);
+    }
+
+    $scope.basicAPICaller = function(UUID, days) {
+        var baseURL = API.baseURL;
+        $location.path(baseURL + UUID + "/" + days);
+    }
+
+
   });
